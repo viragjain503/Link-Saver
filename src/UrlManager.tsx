9d373aa6cd57f2a3181fb94896a15299d2411216
viragjain503 <viragjain503@gmail.com>
2023-10-29 18:14:55 +0530

    MVP requirement is satisfied

---
+++ src/UrlManager.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import './UrlManager.css'; // Import the external CSS file
+import { CopyToClipboard } from 'react-copy-to-clipboard'; // Import the copy-to-clipboard component
+
+function UrlManager() {
+  const [urlInput, setUrlInput] = useState<string>('');
+  const [savedUrls, setSavedUrls] = useState<string[]>([]);
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrlInput(e.target.value);
+  };
+
+  const handleSaveUrl = () => {
+    if (urlInput.trim() !== '') {
+      let alreadyObj: any = [];
+      chrome.storage.local.get(["url"], function (obj) {
+        alreadyObj = obj && obj.url ? obj.url : [];
+        alreadyObj.push(urlInput);
+        chrome.storage.local.set({ "url": alreadyObj }, function () {
+          console.log("Saved data");
+        });
+      });
+
+      setSavedUrls([...savedUrls, urlInput]);
+      setUrlInput('');
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSaveUrl();
+    }
+  };
+
+  useEffect(() => {
+    chrome.storage.local.get(["url"], function (obj) {
+      if (obj.url) {
+        setSavedUrls(obj.url);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>URL Manager</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter URL"
+          value={urlInput}
+          onChange={handleUrlChange}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={handleSaveUrl}>Save</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th>Copy</th> {/* Add a new column for the copy button */}
+          </tr>
+        </thead>
+        <tbody>
+          {savedUrls.map((url, index) => (
+            <tr key={index}>
+              <td>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  {url}
+                </a>
+              </td>
+              <td>
+                <CopyToClipboard text={url}>
+                  <button>Copy</button>
+                </CopyToClipboard>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UrlManager;
